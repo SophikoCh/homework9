4ad766c8e671ed9615fa9601edb3436f53dc548b
sophiko chitadze <chitadzesopho2@gmailcom>
2024-01-25 15:58:21 +0400

    js homeworks

---
+++ main.js
@@ -0,0 +1,56 @@
+// 1 დავალება
+// let number = prompt('please enter a number');
+// let number1 = prompt('please enter second number');
+// alert(`sum: ${parseInt(number) + parseInt(number1)}`);
+// alert(`product: ${parseInt(number) * parseInt(number1)}`);
+
+
+// 2 დავალება
+// alert (5 > 4); //true
+// alert ("apple" > "pineapple"); //false; 
+// alert ("2" > "12"); //true
+// alert (undefined == null); //true
+// alert (undefined === null); //false
+// alert (null == " 0 "); //false
+// alert (null === +"0"); //false
+
+
+// 3 დავალება 
+// let accessAllowed;
+// let name = prompt('Please enter name');
+// if (name === 'Admin') {
+//     let password = prompt('Please enter password');
+
+//     if (password === 'TheMaster') {
+//         alert('Welcome!');
+//     } else if (password === null || password === '') {
+//         alert('Canceled');
+//     } else {
+//         alert('Wrong password');;
+//     }
+// }
+// if (name === '' || name === null) {
+//     alert('Canceled')
+// } else if (name !== 'Admin') {
+//     alert("I don't know you");
+// } 
+
+
+
+// 4 დავალება
+// alert(null || 2 || undefined); // 2         
+// alert(1 && null && 2); // null
+// alert(null || 2 && 3 || 4); // 3
+// alert(!1 && !2 || 3); // 3
+
+// 5 დავალება
+// let number = prompt("Please, enter year")
+// if (number !== "" && !isNaN == number) {
+//     if (number % 4 == 0) {
+//         alert ('It is a leap year')
+//     } else { 
+//         alert ('It is not a leap year')
+//     }
+// } else {
+//     alert ('Please, enter correct year')
+// }
